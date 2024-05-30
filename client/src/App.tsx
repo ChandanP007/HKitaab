1f896d09d4265ef9e2a6a70a0160e3b667ea9377
Chandan Pandey <chandan.dv682@gmail.com>
2024-05-30 23:02:42 +0530

    Update file paths in App.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -6,10 +6,10 @@ import {lazy, Suspense} from 'react'
 //static & components
 import './styles/app.scss'
 import Loader from './components/Main/Loader'
-import Landing from './pages/home/Landing'
-import Home from './pages/home/Home'
-const Register = lazy(()=>import('./pages/auth/register'))
-const Login = lazy(()=>import('./pages/auth/login'))
+import Landing from './pages/Home/Landing'
+import Home from './pages/Home/Home'
+const Register = lazy(()=>import('./pages/Auth/register'))
+const Login = lazy(()=>import('./pages/Auth/login'))
 const MyProfile = lazy(()=>import('./components/Main/MyProfile'))
 
 const App = () => {
@@ -23,7 +23,7 @@ const App = () => {
       <Route path="/me/profile" element={<MyProfile/>} />
       <Route path="/register" element={<Register/>}/>
       <Route path="/login" element={<Login/>}/>
-      <Route path='*' element={<Landing/>}/>
+      {/* <Route path='*' element={<Landing/>}/> */}
     </Routes>
     {/* <Footer/> */}
     </Suspense>
