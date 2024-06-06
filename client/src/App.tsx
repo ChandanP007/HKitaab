88bc5414319c5228abd50f5f91ec5561941f08a4
Chandan Pandey <chandan.dv682@gmail.com>
2024-06-06 18:08:22 +0530

    Update file paths and component names

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -6,10 +6,10 @@ import {lazy, Suspense} from 'react'
 //static & components
 import './styles/app.scss'
 import Loader from './components/Main/Loader'
-import Landing from './pages/Home/Landing'
-import Home from './pages/Home/Home'
-const Register = lazy(()=>import('./pages/Auth/register'))
-const Login = lazy(()=>import('./pages/Auth/login'))
+import Landing from './pages/HomePage/SiteLanding'
+import Home from './pages/HomePage/HomePage'
+const Register = lazy(()=>import('./pages/AuthPages/register'))
+const Login = lazy(()=>import('./pages/AuthPages/login'))
 const MyProfile = lazy(()=>import('./components/Main/MyProfile'))
 
 const App = () => {
@@ -23,7 +23,7 @@ const App = () => {
       <Route path="/me/profile" element={<MyProfile/>} />
       <Route path="/register" element={<Register/>}/>
       <Route path="/login" element={<Login/>}/>
-      <Route path='/landing' element={<Landing/>}/>
+      <Route path='/' element={<Landing/>}/>
     </Routes>
     {/* <Footer/> */}
     </Suspense>
