1d69f864397b5544efde4e5bdfc7bd8a3472e07a
Chandan Pandey <chandan.dv682@gmail.com>
2024-08-18 22:38:37 +0530

    Refactor Transaction component layout and styles

--- client/src/pages/Businesses/Transaction.tsx
+++ client/src/pages/Businesses/Transaction.tsx
@@ -32,11 +32,11 @@ const Transaction = () => {
   return (
     <>
       <main className="flex justify-center">
-        <div className="p-3 w-[50vw] h-[100vh] bg-gray-50 border-dotted border-4 shadow-">
+        <div className="sm:p-3 w-full sm:w-[50vw] h-[100vh] bg-gray-50 border-dotted border-4 shadow-">
           {/* Top section  */}
           <section className="flex flex-col p-5">
             <h1 className="font-semibold text-2xl ">HisaabKitaab.ai</h1>
-            <p className="font-light text-sm">
+            <p className="font-light text-xs sm:text-sm">
               Intelligent ledger management and Shaping Business Transactions
             </p>
           </section>
@@ -45,22 +45,22 @@ const Transaction = () => {
 
           {transactionData && (
             <>
-              <h1 className="font-bold text-xl my-8 mx-3">Transaction ID : {transactionData[0]?.id}</h1>
-              <section className="flex justify-between items-start">
-                <section className="p-6 shadow-sm w-[30vw] bg-white">
-                  <h2 className="font-bold underline">Transaction Summary</h2>
-                  <ul className="p-5 flex flex-col gap-7 text-sm font-light">
+              <h1 className="font-bold sm:text-xl my-8 mx-3">Transaction ID : {transactionData[0]?.id}</h1>
+              <section className="flex flex-col sm:flex-row justify-between items-start">
+                <section className="p-3 sm:p-6 shadow-sm w-full sm:w-[30vw] bg-white">
+                  <h2 className="font-bold underline mb-7 sm:mb-0 text-green-600">Transaction Summary</h2>
+                  <ul className="sm:p-5 flex flex-col gap-5 sm:gap-7 text-sm font-light">
                     {/* <li>Transaction ID  : {transactionData[0].id}</li> */}
-                    <li className="flex flex-col items-start gap-5">
+                    <li className="flex sm:flex-col items-start gap-5">
                       Particulars :{" "}
-                      <span className="text-[16px] font-bold">
+                      <span className="sm:text-[16px] font-bold">
                         {transactionData[0]?.transactionDetails.particulars}{" "}
                       </span>
                     </li>
 
                     <li>
                       Transaction Date :{" "}
-                      <span className=" text-[16px] font-bold">
+                      <span className=" sm:text-[16px] font-bold">
                         {transactionData[0]?.transactionDetails.date}{" "}
                       </span>
                     </li>
@@ -72,24 +72,24 @@ const Transaction = () => {
                         target="_blank"
                         rel="noreferrer"
                       >
-                        <IoDocumentAttachOutline className="text-3xl hover:scale-105 cursor-pointer" />
+                        <IoDocumentAttachOutline className="text-2xl sm:text-3xl hover:scale-105 cursor-pointer" />
                       </a>
                     </li>
                   </ul>
                 </section>
 
-                <section className="p-5 shadow-sm w-[18vw] bg-white">
-                  <h2 className="font-bold underline">GST Information</h2>
+                <section className="p-5 shadow-sm w-full sm:w-[18vw] mt-5 sm:mt-0 bg-white">
+                  <h2 className="font-bold underline text-green-600">GST Information</h2>
                   <ul className="py-5 flex flex-col gap-5 text-sm font-light">
                     <li>
                       Sender GST :{" "}
-                      <span className="text-[16px] font-bold">
+                      <span className="sm:text-[16px] font-bold">
                         {transactionData[0]?.uploadedBy.gst}{" "}
                       </span>{" "}
                     </li>
                     <li>
                       Receiver's GST :{" "}
-                      <span className="text-[16px] font-bold">
+                      <span className="sm:text-[16px] font-bold">
                         {transactionData[0]?.receivedBy.gst}
                       </span>
                     </li>
@@ -100,11 +100,11 @@ const Transaction = () => {
 
 
               <section className="p-5 mt-10 bg-white shadow-md">
-                <h2 className="font-bold underline ">Confirmations</h2>
-                <table className="border p-3 m-5 w-[500px]">
+                <h2 className="font-bold underline text-green-600">Confirmations</h2>
+                <table className="border sm:p-3 m-5 sm:w-[500px]">
                   <thead className="border">
                     {/* <td className="text-sm p-2 font-semibold border">Sender</td> */}
-                    <td className="font-thin text-sm p-2 w-[250px]">
+                    <td className="font-thin text-sm p-2 sm:w-[250px]">
                       Sent & Confirmed by <h2 className="font-bold">{transactionData[0]?.uploadedBy.name}</h2>
                       {/* Sent on {transactionData[0]?.timestamp.split("T")[0]} */}
                     </td>
@@ -144,8 +144,8 @@ const Transaction = () => {
               {
                 transactionData[1]?.gst === transactionData[0]?.receivedBy.gst && transactionData[0]?.confirmations.receiver === "pending" ? (
                 <section className="p-3 mt-4 flex gap-14">
-                <h2 className="text-lg font-semibold"> You have not confirmed the Transaction yet!</h2>
-                <button className="border border-green-500 transition-all hover:bg-green-500 hover:text-white p-2 text-xs rounded-md"
+                <h2 className="sm:text-lg font-semibold"> You have not confirmed the Transaction yet !</h2>
+                <button className="border-2 sm:border border-green-500 transition-all hover:bg-green-500 hover:text-white p-3 sm:p-2 text-xs rounded-md"
                   onClick={()=>handleConfirmation()}
                 >Confirm</button>
                 </section>
